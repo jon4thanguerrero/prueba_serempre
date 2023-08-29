0f19dc92e57858b62f1774080a79e2fbb424e1ee
Jonathan Guerrero <jonathang@merqueo.com>
2023-08-29 18:55:24 -0500

    feat: add send email funcionality

---
+++ resources/views/emails/user-registered.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Correo Usurio registrado</title>
+</head>
+<body>
+    <h1>Hola {{$username}}</h1>
+    <p>Registro realizado con èxito.</p>
+</body>
+</html>
