0dc812912fcee8f960ab394fb823d27ab05e5da1
Jonathan Guerrero <jonathang@merqueo.com>
2023-08-29 18:55:52 -0500

    feat: add user DTO

---
+++ app/DataTransferObject/UserDTO.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataTransferObject;
+
+/**
+ * @author Jonathan Guerrero <jonathan.guerrero.olivera@gmail.com>
+ */
+class UserDTO
+{
+    private string $name;
+    private string $email;
+    private string $password;
+
+    public function setName(string $name) : self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setEmail(string $email) : self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setPassword(string $password) : self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
